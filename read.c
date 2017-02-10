4676358ae3ba74491530b714cf6c3dd01613a882
96Octavian <gogol.logog@hotmail.it>
2017-02-10 12:44:33 +0100

    Complete and working

--- read.c
+++ read.c
@@ -1,24 +1,56 @@
-#include<fcntl.h>       //header file for file operations
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-main(int argc, char*argv[]){
-        if(argc != 2){ //checks if two arguments are present
-                printf("The syntax should be as follows:");
-                printf("\nCommandname filetoread\n");
+#define LINELEN 256
+
+FILE *fp;
+
+void copy_file(int writeLineNumbers);
+
+int main (int argc, char *argv[]) {
+    char cpr[]= "-n";
+    if(argc>2) {
+        perror("Too much arguments");
+        exit(1);
+    }
+    if((fp= fopen("/dev/ttyUSB0", "r"))!=NULL) {
+        if(argc == 2) {
+            if(strcmp(cpr,argv[1])==0) {
+                copy_file(1);
                 exit(1);
-        }
-        int fd, count;
-        char buffer[4096]; //characer buffer to store the bytes
-        fd = open(argv[1], O_RDONLY);
-        if(fd == -1){
-                printf("cannot open file");
+            }
+            else {
+                perror("Unrecognized option");
                 exit(1);
+            }
         }
-        count = read(fd, buffer, sizeof(buffer));
-        while(count > 0){ //displaying the content
-                printf("%s\n", buffer);
-                count = read(fd, buffer, sizeof(buffer));
+        else {
+            copy_file(0);
+            exit(1);
         }
-        exit(0);
+    }
+    else {
+        perror("could not open the file.");
+        exit(1);
+    }
+}
+
+void copy_file (int writeLineNumbers) {
+    char line[LINELEN];
+    int lineno = 1;
+
+    while (fgets(line, LINELEN, fp))
+    {
+        if (writeLineNumbers)
+            printf("%d ", lineno);
+
+        if (fputs(line, stdout) == EOF)
+        {
+            perror("Write to stdout failed.");
+            return;
+        }
+
+        ++lineno;
+    }
 }
